6eb39a328e07da3c2802d5b6f1439413f4a9be96
yao.ni <yao.ni@ele.me>
2016-05-09 20:44:48 +0800

    chore: clock class

---
+++ LifeClock/LifeClock/MinuteHand.swift
@@ -0,0 +1,21 @@
+//
+//  MinuteHand.swift
+//  LifeClock
+//
+//  Created by NiYao on 5/9/16.
+//  Copyright © 2016 SuneBearNi. All rights reserved.
+//
+
+import UIKit
+
+class MinuteHand: Clock {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ LifeClock/LifeClock/SecondHand.swift
@@ -0,0 +1,21 @@
+//
+//  SecondHand.swift
+//  LifeClock
+//
+//  Created by NiYao on 5/9/16.
+//  Copyright © 2016 SuneBearNi. All rights reserved.
+//
+
+import UIKit
+
+class SecondHand: Clock {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
