adc4898a36e1698a4c54ca71493d0a9a8d67056e
yao.ni <yao.ni@ele.me>
2016-05-09 19:29:27 +0800

    init Clock

---
+++ LifeClock/LifeClock/ClockDialView.swift
@@ -0,0 +1,25 @@
+//
+//  ClockDialView.swift
+//  LifeClock
+//
+//  Created by NiYao on 5/9/16.
+//  Copyright © 2016 SuneBearNi. All rights reserved.
+//
+
+import UIKit
+
+class ClockDialView: UIView {
+
+    init(frame: CGRect, backgroundColor: UIColor) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func drawRect(rect: CGRect) {
+        
+    }
+    
+}
